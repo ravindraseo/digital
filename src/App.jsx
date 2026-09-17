532824c2af2ca4bf36cb40ccadc78444c97b81a2
ravindraseo <ravindraniit23@gmail.com>
2026-09-17 19:49:55 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function Logo() {
 
       <div className="logo-text">
         <strong>
-          Digital <b>Ravindraa</b>
+          Digital <b>Ravindra</b>
         </strong>
         <small>Your Growth, Our Digital Strategy</small>
       </div>
